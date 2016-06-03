ca513bfc44dbc91386360c5e6defc38f79cbbf5b
giwigiwi <kostik.forever05@gmail.com>
2016-06-04 00:41:47 +0600

    commit 1

---
+++ Architecture/Assets/PointCreate.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestRaycast : MonoBehaviour
+{
+    public GameObject metkaObj;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void OnMouseDown()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log(hit.collider.name + " " + hit.distance);
+            Instantiate(metkaObj, hit.point, Quaternion.identity);
+
+
+
+        }
+    }
+}
