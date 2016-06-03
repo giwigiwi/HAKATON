14024578cd3e90198f2face1f7ace9dabc281127
Klonium <klonium12@gmail.com>
2016-06-04 00:28:35 +0600

    Commit scene monitor complete

--- Architecture/Assets/PointPath.cs
+++ Architecture/Assets/PointPath.cs
@@ -1,8 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEditorInternal;
 
-[ExecuteInEditMode]
 public class PointPath : MonoBehaviour {
 
     public GameObject[] pathLine;
@@ -11,32 +9,12 @@ public class PointPath : MonoBehaviour {
     private TextMesh textM;
     private GameObject gmText;
 
-   public void Start()
+    public void Start()
     {
-        if (transform.GetChildCount() < 1)
-        {
-            gmText = (GameObject)Instantiate(text, transform.position, Quaternion.identity);
-            gmText.transform.parent = transform;
-            textM = gmText.GetComponent<TextMesh>();
-        }
-        else
-        {
-            gmText = transform.GetChild(0).gameObject;
-            gmText.transform.parent = transform;
-            textM = gmText.GetComponent<TextMesh>();
-        }
-
     }
-
     public void Update()
     {
-        if (monitoring)
-        {
 
-            for (int i = 0; i < pathLine.Length; i++)
-                Debug.DrawLine(transform.position, pathLine[i].transform.position);
-            textM.text = gameObject.name;
-        }
 
     }
 
